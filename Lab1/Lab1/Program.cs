ca528806bf8c217d3c36882df8e50d5f47d0d19c
Bagynur <baghnurkabykenov@gmail.com>
2019-02-04 22:13:12 +0600

    Second commit

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -4,12 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1
+namespace task2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int cnt = 0;
+            int a = int.Parse(Console.ReadLine());
+            int [] bk = new int[a];
+            List<int> BB = new List<int>();
+
+            for (int i = 0; i < a; i++) 
+            {
+                bk[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a; i++) 
+            {
+                int c = 0;
+                if ((bk[i] != 1) && (bk[i] != 0)) 
+                {
+                    for (int j = 2; j < bk[i]; i++) 
+                    {
+                        if (bk[i]%j==0) 
+                        {
+                            c++;
+                        }
+                    }
+                    if (c == 0) 
+                    cnt++;
+                    BB.Add(bk[i]);
+                }
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < BB.Count(); i++)
+            {
+                Console.Write(BB[i] + " ");
+            }
+
         }
+
     }
 }
--- Lab1/task2/Program.cs
+++ Lab1/task2/Program.cs
@@ -10,6 +10,26 @@ namespace task2
     {
         static void Main(string[] args)
         {
+            int cnt = 0;
+            int a = int.Parse(Console.ReadLine());
+            int bk[] = new int[a];
+            List<int> BB = new List<int>();
+
+            for (int i = 0; i < a; i++) ;
+            {
+                bk[]= int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a; i++) ;
+            {
+                int c = 0;
+                if ((bk[i] != 1) && (bk[i] != 0)) ;
+                {
+                    for (int j = 2; j < bk[i]; i++) ;
+                    {
+                    }
+                }
+            }
         }
     }
 }
