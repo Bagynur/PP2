6bf621e083a932f03e4fa02a838357b746d8b7f8
Bagynur <baghnurkabykenov@gmail.com>
2019-02-09 19:26:18 +0600

    4 commit

--- Lab1/task3/Program.cs
+++ Lab1/task3/Program.cs
@@ -4,26 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task3
+namespace ConsoleApp7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            string[] arr = Console.ReadLine();
-            int[] a = new int[n * 3];
-            for (int i = 0; i < n; i++)
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n]; 
+            string s = Console.ReadLine(); 
+            string[] ss = s.Split(); 
+            for (int i = 0; i < n; i++) 
             {
-                a[i * 3] = int.Parse(arr[i]);
-                a[i * 3 + 1] = a[i * 3];
-                a[i * 3 + 2] = a[i * 3];
+                a[i] = int.Parse(ss[i]); 
+                for (int j = 0; j < 2; j++) 
+                {
+                    Console.Write(a[i] + " "); 
+                }
             }
-            for (int i = 0; i < n * 3; i++)
-            {
-                Console.Write(a[i] + " ");
-            }
-            Console.ReadKey();
+            Console.ReadKey(); 
         }
     }
 }
