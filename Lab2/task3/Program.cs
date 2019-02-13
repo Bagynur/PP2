b792ffa7dea22a9d3f020bd662a854c106a07acf
Bagynur <baghnurkabykenov@gmail.com>
2019-02-13 22:47:56 +0600

    2 commit

--- Lab2/task3/Program.cs
+++ Lab2/task3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace task3
 {
@@ -10,6 +11,29 @@ namespace task3
     {
         static void Main(string[] args)
         {
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Users\kabyk\Desktop\PP2_2019");
+            Director(dirinfo, 0);
+        }
+        public static void Director(DirectoryInfo dir, int space)
+        {
+            foreach(DirectoryInfo y in dir.GetDirectories())
+            {
+                addspace(space);
+                Console.WriteLine(y.Name);
+                Director(y,space + 1);
+            }
+            foreach(FileInfo x in dir.GetFiles())
+            {
+                addspace(space);
+                Console.WriteLine(x.Name);
+            }
+        }
+        public static void addspace(int space)
+        {
+            for(int i=0; i<space; i++)
+            {
+                Console.Write("    ");
+            }
         }
     }
 }
