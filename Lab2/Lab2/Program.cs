e9eb8eaaeaf7727a78188d8848f9b2216f2ab553
Bagynur <baghnurkabykenov@gmail.com>
2019-02-14 15:27:20 +0600

    2 commit

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -10,24 +10,25 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            string s = System.IO.File.ReadAllText(@"C:\Users\kabyk\Desktop\PP2_2019\Lab2\Lab2\text.txt.txt");
-            bool ok = true;
-            int k = s.Length;
-            for (int i=0; i<k/2; i++)
+            string s = System.IO.File.ReadAllText(@"C:\Users\kabyk\Desktop\PP2_2019\Lab2\Lab2\text.txt.txt"); // choose a file where we will read numbers
+            bool ok = true; // create bool ok and write that it equal to true
+            int k = s.Length; // int k equal to length of string s, to run cycle for
+            for (int i=0; i<k/2; i++) // start of cycle
             {
-                if (s[i] != s[k - 1 - i])
+                if (s[i] != s[k - 1 - i]) // if first half of the number is not equal to another part
                 {
-                    ok = false;
-                    break;
+                    ok = false; // ok will be equal to the false
+                    break; // it stop working further
                 }
 
             }
-             if (ok == true){
-                Console.WriteLine("Yes");
+             if (ok == true) // if ok is equal to true 
+            { 
+                Console.WriteLine("Yes"); // then print yes
             }
             else
             {
-                Console.WriteLine("No");
+                Console.WriteLine("No"); // else no
             }
         }
     }
