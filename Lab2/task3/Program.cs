098879701b383fe0df43245ea662dbbcdc22a9cb
Bagynur <baghnurkabykenov@gmail.com>
2019-02-14 15:32:27 +0600

    3 commit

--- Lab2/task3/Program.cs
+++ Lab2/task3/Program.cs
@@ -11,24 +11,25 @@ namespace task3
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Users\kabyk\Desktop\PP2_2019");
-            Director(dirinfo, 0);
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Users\kabyk\Desktop\PP2_2019"); //path of folder
+            Director(dirinfo, 0); //use the function
         }
-        public static void Director(DirectoryInfo dir, int space)
+    }
+        public static void Director(DirectoryInfo dir, int space) //fuction to output list of directories and files
         {
             foreach(DirectoryInfo y in dir.GetDirectories())
             {
-                addspace(space);
-                Console.WriteLine(y.Name);
-                Director(y,space + 1);
+                addspace(space); //use the addspace func
+                Console.WriteLine(y.Name);  //output names of folders
+                Director(y,space + 1); //using recursion repeat while the directories ends
             }
             foreach(FileInfo x in dir.GetFiles())
             {
-                addspace(space);
-                Console.WriteLine(x.Name);
+                addspace(space); //adds spaces to file
+                Console.WriteLine(x.Name); // outputs names of files
             }
         }
-        public static void addspace(int space)
+        public static void addspace(int space) //function to add spaces
         {
             for(int i=0; i<space; i++)
             {
