c3d4329bb1bacfcc3e0a47be9634942b2781f338
Bagynur <baghnurkabykenov@gmail.com>
2019-02-14 15:38:39 +0600

    3 commit

--- Lab2/task4/Program.cs
+++ Lab2/task4/Program.cs
@@ -11,23 +11,23 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
-            string filename = Console.ReadLine();
-            string sourcePath = @"C:\Users\kabyk\Desktop\PP2_2019\Lab2\task4";
-            string targetPath = @"C:\Users\kabyk\Desktop\PP2_2019\Lab2\task3";
+            string filename = Console.ReadLine(); // firstly we need to give name to the file 
+            string sourcePath = @"C:\Users\kabyk\Desktop\PP2_2019\Lab2\task4"; // directory where we creare the original file
+            string targetPath = @"C:\Users\kabyk\Desktop\PP2_2019\Lab2\task3"; // directory where will be located copied file
             string sourceFile = Path.Combine(sourcePath, filename);
             string destFile = Path.Combine(targetPath, filename);
-            File.Create(sourcePath + @"\" + filename).Dispose();
-            Console.WriteLine("Press SPACE to copy the file");
+            File.Create(sourcePath + @"\" + filename).Dispose(); // creating a file
+            Console.WriteLine("Press SPACE to copy the file"); // some guides
             ConsoleKeyInfo pressedKey = Console.ReadKey();
-            if (pressedKey.Key == ConsoleKey.Spacebar)
+            if (pressedKey.Key == ConsoleKey.Spacebar) // if we press SPACE then our file copies to the targetPath
             {
                 File.Copy(sourceFile, destFile);
             }
-            Console.WriteLine("Press Backspace to delete the file");
+            Console.WriteLine("Press Backspace to delete the file"); // some guides
             ConsoleKeyInfo pressedKey2 = Console.ReadKey();
             if (pressedKey2.Key == ConsoleKey.Backspace)
             {
-                File.Delete(sourcePath + @"\" + filename);
+                File.Delete(sourcePath + @"\" + filename); // if we press BACKSPACE then our file deletes from the sourcePath
             }
 
         }
